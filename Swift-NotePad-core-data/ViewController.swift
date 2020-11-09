2e104ef40c7908ced42c5b3d40026f28190e16e7
Subhrajyoti <subha.ionic3@gmail.com>
2020-11-09 20:56:12 +0530

    added simple table view's initial codebase

--- Swift-NotePad-core-data/ViewController.swift
+++ Swift-NotePad-core-data/ViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var notesArray: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
     }
 
 
